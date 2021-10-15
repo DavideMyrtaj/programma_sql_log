d55fa76017b96be699e2f74b3ccde784c1b03dbe
DavideMyrtaj <75247700+DavideMyrtaj@users.noreply.github.com>
2021-10-15 20:51:22 +0200

    tabella prodotti e log

--- tabellaLog/Form1.Designer.cs
+++ tabellaLog/Form1.Designer.cs
@@ -28,151 +28,173 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.azione1 = new System.Windows.Forms.Button();
-            this.azione2 = new System.Windows.Forms.Button();
-            this.azione3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.creaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabelleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dbToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(40, 35);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(115, 34);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "crea tabella log";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.EnabledChanged += new System.EventHandler(this.button1_EnabledChanged);
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Location = new System.Drawing.Point(666, 35);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(111, 73);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Accesso";
-            // 
-            // button3
-            // 
-            this.button3.Enabled = false;
-            this.button3.Location = new System.Drawing.Point(6, 43);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(99, 22);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Log Out";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button2
-            // 
-            this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(6, 19);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(99, 22);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Log In";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.EnabledChanged += new System.EventHandler(this.button2_EnabledChanged);
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(505, 29);
+            this.label1.Location = new System.Drawing.Point(20, 20);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 2;
+            this.label1.TabIndex = 0;
             this.label1.Text = "Username";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(505, 78);
+            this.label2.Location = new System.Drawing.Point(20, 68);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(53, 13);
-            this.label2.TabIndex = 3;
+            this.label2.TabIndex = 2;
             this.label2.Text = "Password";
             // 
-            // textBox1
+            // groupBox1
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(508, 49);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(142, 20);
-            this.textBox1.TabIndex = 4;
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 123);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log in";
             // 
-            // textBox2
+            // button1
             // 
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(508, 94);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(142, 20);
-            this.textBox2.TabIndex = 5;
+            this.button1.Location = new System.Drawing.Point(233, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 22);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Accedi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // azione1
+            // button2
             // 
-            this.azione1.Enabled = false;
-            this.azione1.Location = new System.Drawing.Point(62, 223);
-            this.azione1.Name = "azione1";
-            this.azione1.Size = new System.Drawing.Size(143, 36);
-            this.azione1.TabIndex = 6;
-            this.azione1.Text = "ciao";
-            this.azione1.UseVisualStyleBackColor = true;
-            this.azione1.Click += new System.EventHandler(this.azione1_Click);
+            this.button2.Location = new System.Drawing.Point(366, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 22);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Registrati";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
-            // azione2
+            // label3
             // 
-            this.azione2.Enabled = false;
-            this.azione2.Location = new System.Drawing.Point(294, 223);
-            this.azione2.Name = "azione2";
-            this.azione2.Size = new System.Drawing.Size(143, 36);
-            this.azione2.TabIndex = 7;
-            this.azione2.Text = "Numero randomico 1-2000";
-            this.azione2.UseVisualStyleBackColor = true;
-            this.azione2.Click += new System.EventHandler(this.azione2_Click);
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(347, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "o";
             // 
-            // azione3
+            // button3
             // 
-            this.azione3.Enabled = false;
-            this.azione3.Location = new System.Drawing.Point(508, 223);
-            this.azione3.Name = "azione3";
-            this.azione3.Size = new System.Drawing.Size(143, 36);
-            this.azione3.TabIndex = 8;
-            this.azione3.Text = ":)";
-            this.azione3.UseVisualStyleBackColor = true;
-            this.azione3.Click += new System.EventHandler(this.azione3_Click);
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(233, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 22);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Log out";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(40, 187);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 36);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Inserimento ";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.creaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(526, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // creaToolStripMenuItem
+            // 
+            this.creaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tabelleToolStripMenuItem,
+            this.dbToolStripMenuItem});
+            this.creaToolStripMenuItem.Name = "creaToolStripMenuItem";
+            this.creaToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.creaToolStripMenuItem.Text = "crea";
+            this.creaToolStripMenuItem.Click += new System.EventHandler(this.creaToolStripMenuItem_Click);
+            // 
+            // tabelleToolStripMenuItem
+            // 
+            this.tabelleToolStripMenuItem.Name = "tabelleToolStripMenuItem";
+            this.tabelleToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tabelleToolStripMenuItem.Text = "tabelle";
+            this.tabelleToolStripMenuItem.Click += new System.EventHandler(this.tabelleToolStripMenuItem_Click);
+            // 
+            // dbToolStripMenuItem
+            // 
+            this.dbToolStripMenuItem.Name = "dbToolStripMenuItem";
+            this.dbToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dbToolStripMenuItem.Text = "db";
+            this.dbToolStripMenuItem.Click += new System.EventHandler(this.dbToolStripMenuItem_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.azione3);
-            this.Controls.Add(this.azione2);
-            this.Controls.Add(this.azione1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(526, 280);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,17 +202,20 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button azione1;
-        private System.Windows.Forms.Button azione2;
-        private System.Windows.Forms.Button azione3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem creaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tabelleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dbToolStripMenuItem;
     }
 }
 
--- tabellaLog/Form1.cs
+++ tabellaLog/Form1.cs
@@ -13,7 +13,7 @@ namespace tabellaLog
 {
     public partial class Form1 : Form
     {
-        SqlConnection Conn = new SqlConnection("Data Source=DESKTOP-MVIRBBM;Initial Catalog=sium;Integrated Security=True");
+        SqlConnection Conn = new SqlConnection("Data Source=DESKTOP-MVIRBBM;Initial Catalog=sium2;Integrated Security=True");
         SqlCommand cmd;
         Account account;
 
@@ -22,87 +22,132 @@ namespace tabellaLog
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+       
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
         {
-            string query = @"CREATE TABLE dbo.LogTable(ID int IDENTITY(1,1) NOT NULL,"+
-                            "Utente nchar(100) NULL,Ora datetime NULL,Azione nchar(100) NULL,"+
-                            "Valore nchar(50) NULL);";
+            //verifica se esistono le tabelle e la crea
+
+            CreaTabelle();
+
+        }
+
+        void CreaTabelle()
+        {
+            string query = "IF OBJECT_ID('dbo.LogTable') IS NOT NULL begin PRINT 'tabella log esiste' end else begin CREATE TABLE LogTable(ID int IDENTITY(1,1) NOT NULL,Utente nchar(100) NULL,Ora datetime NULL,Azione nchar(100) NULL,Valore nchar(50) NULL) end IF OBJECT_ID('dbo.Account') IS NOT NULL begin PRINT 'tabella account esiste' end else begin Create Table Account(Username nvarchar(100) PRIMARY KEY, Password nvarchar(100) NOT NULL) end IF OBJECT_ID('dbo.TabellaProdotti') IS NOT NULL begin PRINT 'tabella prodotti esiste' end else begin Create Table TabellaProdotti(Prodotto nvarchar(50) PRIMARY KEY, Quantità int NOT NULL, Prezzo float NOT NULL) end; ";
             cmd = new SqlCommand(query, Conn);
             try
             {
                 Conn.Open();
                 cmd.ExecuteNonQuery();
-                MessageBox.Show("tabella di log creata con successo");
-                
-                button1.Enabled = false;
-            }
-            catch (SqlException errore)
-            {
+                MessageBox.Show("creazione tabelle riuscita");
 
-                MessageBox.Show($"si è verificato un errore: {errore.Message}");
             }
+            catch (SqlException error)
+            {
+                MessageBox.Show($"si è verificato un errore durante la creazione delle tabelle:\n{error.Message}");
+
+            }
+            cmd.Dispose();
             Conn.Close();
         }
+      
 
-        private void button1_EnabledChanged(object sender, EventArgs e)
-        {
-            if (button1.Enabled==true)
-            {
-                button2.Enabled = false;
-                button3.Enabled = false;
-            }
-            else
-            {
-                button2.Enabled = true;
-                
-            }
-        }
+       
 
-        private void Form1_Load(object sender, EventArgs e)
+       
+
+        
+
+       
+
+        /// <summary>
+        /// inserimento nella tabella di log dell'azione effettuata
+        /// </summary>
+        /// <param name="azione"></param>
+        /// <param name="valore"></param>
+        void InserimentoDB(string azione, string valore)
         {
-            //verifica se esiste tabella
-            string pino;
-            string query = @"SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'LogTable';";
-            cmd = new SqlCommand(query, Conn);
-            Conn.Open();
-            SqlDataReader reader=cmd.ExecuteReader();
             
-            if (reader.HasRows)
-            {
-                button1.Enabled = false;
-                button2.Enabled = true;
-                textBox1.Enabled = true;
-                textBox2.Enabled = true;
-            }
-                
-            else
-            {
-                button1.Enabled = true;
-            }
-                
-            Conn.Close();
+            string query = $"INSERT INTO LogTable (Utente,Ora,Azione,Valore) VALUES('{account.Username}','{DateTime.Now}','{azione}','{valore}');";
+            cmd = new SqlCommand(query, Conn);
 
+            try
+            {
+                Conn.Open();
+                cmd.ExecuteNonQuery();
+                
+
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show($"errore durante l'aggiunta nella tabella log:\n{error.Message}");
+
+            }
+            cmd.Dispose();
+            Conn.Close();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click_1(object sender, EventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(textBox1.Text)| string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("I campi non possono essere vuoti");
+                return;
+            }
+            string query = $"if(not EXISTS(Select * from dbo.Account where Username='{textBox1.Text}'))  begin insert into Account(Username,Password) Values('{textBox1.Text}','{textBox2.Text}')end else	begin print('username già esistente') end;";
+            cmd = new SqlCommand(query, Conn);
+            try
+            {
+                Conn.Open();
+
+                if (cmd.ExecuteNonQuery() != -1)
+                {
+                    MessageBox.Show("registrazione avvenuta con successo");
+                }
+                else
+                {
+                    MessageBox.Show("username non valido");
+                }
+               
+                
+            }
+            catch (SqlException err)
+            {
+                MessageBox.Show(err.Message);
+                
+            }
+            cmd.Dispose();
+            Conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text)|string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("i campi non possono essere vuoti");
+                return;
+            }
             string query = $"SELECT * FROM dbo.Account WHERE Username='{textBox1.Text}' AND Password='{textBox2.Text}';";
             cmd = new SqlCommand(query, Conn);
             Conn.Open();
             SqlDataReader reader = cmd.ExecuteReader();
             if (reader.HasRows)
             {
-
+                
                 account = new Account();
                 reader.Read();
                 account.Username = reader["Username"].ToString();
-                account.Password = reader["Password"].ToString();
-                
                 MessageBox.Show("accesso eseguito correttamente");
+                button1.Enabled = false;
                 button2.Enabled = false;
                 button3.Enabled = true;
                 textBox1.Enabled = false;
                 textBox2.Enabled = false;
+                reader.Close();
+                cmd.Dispose();
                 Conn.Close();
                 InserimentoDB("LOG IN", "");
             }
@@ -111,71 +156,80 @@ namespace tabellaLog
                 MessageBox.Show("credenziali non valide");
                 Conn.Close();
             }
-                
-            
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void tabelleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreaTabelle();
+        }
+
+        private void dbToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SqlConnection creaDb = new SqlConnection("Data Source=DESKTOP-MVIRBBM;Initial Catalog=master;Integrated Security=True");
+            string query = "CREATE DATABASE sium2";
+            cmd = new SqlCommand(query, creaDb);
+            try
+            {
+                creaDb.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("creazione db riuscita");
+
+            }
+            catch (SqlException error)
+            {
+                MessageBox.Show($"si è verificato un errore durante la creazione del db:\n{error.Message}");
+
+            }
+            cmd.Dispose();
+            creaDb.Close();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
         {
             InserimentoDB("LOG OUT", "");
             account = null;
             button3.Enabled = false;
             button2.Enabled = true;
+            button1.Enabled = true;
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
             MessageBox.Show("log out eseguito correttamente");
         }
 
-        private void button2_EnabledChanged(object sender, EventArgs e)
+        private void button4_Click(object sender, EventArgs e)
         {
-            if (button2.Enabled==true)
+            if (account==null)
             {
-                azione1.Enabled = false;
-                azione2.Enabled = false;
-                azione3.Enabled = false;
-                textBox1.Enabled = true;
-                textBox2.Enabled = true;
+                MessageBox.Show("devi effettuare prima l'accesso");
+                return;
             }
-            else
+            try
             {
-                azione1.Enabled = true;
-                azione2.Enabled = true;
-                azione3.Enabled = true;
-                textBox1.Enabled = false;
-                textBox2.Enabled = false;
+                Conn.Open();
+                insert form = new insert(Conn,account);
+                form.ShowDialog();
+                Conn.Close();
+                
             }
-        }
+            catch (SqlException err)
+            {
+                MessageBox.Show(err.Message);
+                Conn.Close();
 
-        private void azione1_Click(object sender, EventArgs e)
-        {
-            InserimentoDB("inserimento", "ciao");
-            MessageBox.Show("valore aggiunto correttamente");
-        }
-
-        private void azione2_Click(object sender, EventArgs e)
-        {
-            Random r = new Random();
-            InserimentoDB("Inserimento", r.Next(1, 2000).ToString());
-            MessageBox.Show("valore aggiunto correttamente");
-        }
-
-        private void azione3_Click(object sender, EventArgs e)
-        {
-            InserimentoDB("inserimento", ":)");
-            MessageBox.Show("valore aggiunto correttamente");
-        }
-        void InserimentoDB(string azione, string valore)
-        {
+            }
             
-            string query = $"INSERT INTO LogTable (Utente,Ora,Azione,Valore) VALUES('{account.Username}','{DateTime.Now}','{azione}','{valore}');";
-            cmd = new SqlCommand(query, Conn);
-            Conn.Open();
-            cmd.ExecuteNonQuery();
-            Conn.Close();
+        }
+
+        private void creaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
+
+    //classe utilizzata per contenere i dati dell'utente che ha fatto l'accesso
     public class Account
     {
         public string Username = "";
-        public string Password = "";
     }
     
 }
